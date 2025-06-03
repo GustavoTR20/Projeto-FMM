cac7fa7185b083504b31b0737e6af5a410c0c9d2
GustavoTaveiraReis <gustavo.taveirareis@student.griffith.ie>
2025-06-03 21:29:53 +0100

    Pagina principal com um layout moderno

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.querySelector('.hamburger').addEventListener('click', () => {
 document.querySelector('.close-btn').addEventListener('click', () => {
   document.getElementById('sideMenu').classList.remove('active');
 });
+
+const track = document.getElementById('carouselTrack');
+const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev');
+
+btnNext.addEventListener('click', () => {
+  track.scrollBy({ left: 320, behavior: 'smooth' });
+});
+
+btnPrev.addEventListener('click', () => {
+  track.scrollBy({ left: -320, behavior: 'smooth' });
+});
+
